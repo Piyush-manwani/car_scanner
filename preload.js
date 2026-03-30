a87d09141f8566cc4bf1cb263bb5006161a39d8f
Piyush-manwani <piyush.manwani@indusschool.com>
2026-03-30 15:49:37 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  diagnose: (data) => ipcRenderer.invoke('diagnose', data)
+  diagnose:       (data)   => ipcRenderer.invoke('diagnose', data),
+  settingsLoad:   ()       => ipcRenderer.invoke('settings-load'),
+  settingsSave:   (data)   => ipcRenderer.invoke('settings-save', data),
+  settingsPath:   ()       => ipcRenderer.invoke('settings-path'),
+  profileLoad:    ()       => ipcRenderer.invoke('profile-load'),
+  profileSave:    (data)   => ipcRenderer.invoke('profile-save', data),
+  profilePath:    ()       => ipcRenderer.invoke('profile-path'),
+  profileEditor:  ()       => ipcRenderer.invoke('profile-editor'),
+  profileVehicle: ()       => ipcRenderer.invoke('profile-vehicle'),
 });
